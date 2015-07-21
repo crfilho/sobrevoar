cfa07da5af6e885f7eaa1b3ef2889e23ac2f3d0e
CARLOS SANTILLI <carlos.santilli@gmail.com>
2015-07-21 03:23:11 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+(function($){
+    $(document).ready(function(){
+
+        var homeContentBlock = $('.home-content-block');
+        var homeSliderHeight = $('.slider-height');
+        var homeSlider = $('#owlimgslider');
+        var navToggle = $('.navbar-toggle');
+        var iconBar = $('.icon-bar');
+
+        // GOOO
+        setTimeout(function(){$("body").removeClass("fade");}, 1000);
+
+        //////// CAROUSEL
+        $('.carousel').carousel();
+
+        //$(window).load(function() {
+        //    if ($(window).width() > 1025) {
+        //        $('#sidebar').removeClass('.affix')
+        //    }
+        //});
+
+
+        /////PARALAX OFFSET
+
+        $(window).load(function() {
+            homeContentBlock.offset({
+                top: homeSliderHeight.outerHeight()
+            });
+            if ($(window).width() < 481) {
+                homeContentBlock.offset({
+                    top: homeSliderHeight.outerHeight() + 30
+                });
+            }
+
+            navToggle.click(function(){
+                $(this).toggleClass( 'nav-close' );
+                iconBar.toggle();
+            })
+
+        });
+
+        $(window).resize(function() {
+            homeContentBlock.css('top', homeSliderHeight.height() + "px");
+            if ($(window).width() < 481) {
+                homeContentBlock.css('top', homeSliderHeight.height() + 30 + "px");
+            }
+        });
+
+    });
+
+})(jQuery)
+
+
+$(window).load(function() {
+    if ($(window).width() > 1025) {
+        $('#sidebar').affix({
+            offset: {
+                top: $('.product-intro').outerHeight() - 160
+                //bottom: $( '#page' ).height()+($('#product-block').outerHeight())
+            }
+        });
+    }
+});
+
+$(window).scroll(function(){
+        if ($(this).scrollTop() > ($('.product-images').innerHeight())+40) {
+            $('.affix').css({'top':(($('.product-images').outerHeight() - ($('#sidebar').outerHeight()) - 42))+'px','position':'relative'});
+        }
+        else if ($(this).scrollTop() < $('.product-intro').innerHeight() + 50) {
+            $('.affix').css({'top': '0', 'position': 'relative'});
+        }
+        else {
+            $('.affix').css({'top':'120px','position':'fixed'});
+        }
+    });
+
+
+
+
+
+
+
---
+++ js/plugins.js
@@ -0,0 +1,101 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+
+    $(document).ready(function() {
+        var method;
+        var noop = function () {};
+        var methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+            'timeStamp', 'trace', 'warn'
+        ];
+        var length = methods.length;
+        var console = (window.console = window.console || {});
+
+        while (length--) {
+            method = methods[length];
+
+            // Only stub undefined methods.
+            if (!console[method]) {
+                console[method] = noop;
+            }
+        }
+       
+
+        /////PRODUCT PAGE AFFIX CODE
+
+        //$('#sidebar').affix({
+        //    //offset: {
+        //    //    top: $('.product-intro').outerHeight()
+        //    //}
+        //});
+        //
+        //$(window).scroll(function(){
+        //    if ($(this).scrollTop() > $('.product-images').outerHeight()+100)
+        //    {
+        //        $('.affix').css({'top':(($('.product-images').height() + 220))+'px','position':'absolute'});
+        //    }
+        //    if ($(this).scrollTop() < $('.product-images').height() + 100)
+        //    {
+        //        $('.affix').css({'top':'125px','position':'fixed'});
+        //    }
+        //    if ($(this).scrollTop() < $('.product-intro').height() + 125) {
+        //        $('.affix').css({'top': '0', 'position': 'relative'});
+        //    }
+        //});
+
+
+        ///////NAVBAR TOGGLE - HIDE HAMBURGER ADD CLASS FOR X
+        //
+        //$(window).load(function() {
+        //    $('.navbar-toggle').click(function(){
+        //        $( this ).toggleClass( 'nav-close' );
+        //        $('.icon-bar').toggle();
+        //    });
+        //
+        //});
+        //
+        ///////NAVBAR FULL SCREEN WIDTH ON IPAD
+        //
+        //$(window).load(function() {
+        //    if ($(window).width() < 1025) {
+        //        $('#navbar').removeClass('container');
+        //    }
+        //});
+        //
+        ///////PARALAX OFFSET
+        //
+        //$(window).load(function() {
+        //    if ($(window).width() < 1800) {
+        //        $('#slide-2').offset({
+        //            top: $('#owlimgslider').outerHeight() + 50
+        //        });
+        //    }
+        //});
+        //
+        //////////OWL CAROUSEL
+        //$("#owlimgslider").owlCarousel({
+        //    navigation : false, // Show next and prev buttons
+        //    slideSpeed : 200,
+        //    paginationSpeed : 300,
+        //    singleItem : true,
+        //    autoPlay : true
+        //});
+
+        ////refresh page on browser resize
+        //$(window).bind('resize', function(e)
+        //{
+        //    if ($(window).width() > 767) {
+        //
+        //        if (window.RT) clearTimeout(window.RT);
+        //        window.RT = setTimeout(function () {
+        //            this.location.reload(false);
+        //            /* false to get page from cache */
+        //        }, 200);
+        //    }
+        //});
+});
+}());
+
+// Place any jQuery/helper plugins in here.
